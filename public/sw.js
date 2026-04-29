0293774a56f9afa2f633d3f6a48ce8c296178378
Juanjo3222 <86850741+Juanjo3222@users.noreply.github.com>
2026-04-29 06:12:24 -0500

    Refactor service worker event listeners for brevity

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,19 @@
 importScripts("/scram/scramjet.all.js");
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(self.skipWaiting());
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
+self.addEventListener("install", (e) => e.waitUntil(self.skipWaiting()));
+self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
 
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
+self.addEventListener("message", (event) => {
+  if (event.data?.type === "init") {
+    scramjet.config = event.data.config;
+  }
+});
+
 self.addEventListener("fetch", (event) => {
-  if (scramjet.route(event)) {
+  if (scramjet.config && scramjet.route(event)) {
     event.respondWith(scramjet.fetch(event));
   }
 });
